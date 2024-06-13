eaaec8414e62027da0a1e785b0b3fb2b62013b69
SurjeetMandal <surjeetmandal698@gmail.com>
2024-06-14 01:08:17 +0530

    submitting code-Fri Jun 14 2024 01:08:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
 
-import React from "react";
-import './../styles/App.css';
-
+// Define the App component as a functional component
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    // State to manage the login status
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Function to handle login
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    }
+
+    return (
+        <div>
+            <h1>{isLoggedIn ? 'Welcome!' : 'Please log in'}</h1>
+            {/* Pass the login status and login handler to the LoginForm component */}
+            <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+        </div>
+    );
 }
 
-export default App
+export default App;
+
